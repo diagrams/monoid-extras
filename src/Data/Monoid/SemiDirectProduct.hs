bbd0abefee8a3e3e9ad6e7ffd3161c7c6325c6b4
Brent Yorgey <byorgey@gmail.com>
2016-07-08 16:56:18 -0500

    cleanup and documentation edits

--- src/Data/Monoid/SemiDirectProduct.hs
+++ src/Data/Monoid/SemiDirectProduct.hs
@@ -1,24 +1,29 @@
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TupleSections         #-}
-{-# LANGUAGE CPP                   #-}
 
 module Data.Monoid.SemiDirectProduct
        ( Semi, quotient, inject, embed
        ) where
 
 #if !MIN_VERSION_base(4,8,0)
-import Data.Monoid
+import           Data.Monoid
 #endif
 
-import Data.Monoid.Action
+import           Data.Monoid.Action
 
--- | The semi-direct product of monoids @s@ and @m@. When the monoid
--- @m@ acts on the monoid @s@, this type acquires a monoid structure.
--- We call the monoid @m@ the quotient monoid and the monoid @s@ the
--- sub-monoid of the semi-direct product. The semi-direct product
--- @Semi s m@ is an extension of the monoid @s@ with @m@ being the
--- quotient.
+-- | The semi-direct product of monoids @s@ and @m@, which is a monoid
+--   when @m@ acts on @s@. Structurally, the semi-direct product is
+--   just a pair @(s,m)@.  However, the monoid instance is different.
+--   In particular, we have
+--
+-- > (s1,m1) <> (s2,m2) = (s1 <> (m1 `act` s2), m1 <> m2)
+--
+--   We call the monoid @m@ the quotient monoid and the monoid @s@ the
+--   sub-monoid of the semi-direct product. The semi-direct product
+--   @Semi s m@ is an extension of the monoid @s@ with @m@ being the
+--   quotient.
 newtype Semi s m = Semi { unSemi :: (s,m) }
 
 
@@ -43,8 +48,8 @@ inject :: Monoid m => s -> Semi s m
 inject = Semi . (,mempty)
 
 -- | The semi-direct product gives a split extension of @s@ by
--- @m@. This allows us to embed @m@ into the semi-direct product. This
--- is the embedding map. The quotient and embed maps should satisfy
--- the equation @quotient . embed = id@.
+--   @m@. This allows us to embed @m@ into the semi-direct
+--   product. This is the embedding map. The quotient and embed maps
+--   should satisfy the equation @quotient . embed = id@.
 embed :: Monoid s => m -> Semi s m
 embed = Semi . (mempty,)
--- src/Data/Monoid/SemiDirectProduct/Strict.hs
+++ src/Data/Monoid/SemiDirectProduct/Strict.hs
@@ -1,28 +1,33 @@
 -- | A strict version of the semi-direct product. If a monoid m acts
--- on s then this version of the semi-direct product is strict in the
--- m-portion of the semi-direct product.
+--   on s then this version of the semi-direct product is strict in
+--   the m-portion of the semi-direct product.
 
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TupleSections         #-}
-{-# LANGUAGE CPP                   #-}
 
 module Data.Monoid.SemiDirectProduct.Strict
        ( Semi, quotient, inject, embed
        ) where
 
 #if !MIN_VERSION_base(4,8,0)
-import Data.Monoid
+import           Data.Monoid
 #endif
 
-import Data.Monoid.Action
+import           Data.Monoid.Action
 
--- | The semi-direct product of monoids @s@ and @m@. When the monoid
--- @m@ acts on the monoid @s@, this type acquires a monoid structure.
--- We call the monoid @m@ the quotient monoid and the monoid @s@ the
--- sub-monoid of the semi-direct product. The semi-direct product
--- @Semi s m@ is an extension of the monoid @s@ with @m@ being the
--- quotient.
+-- | The semi-direct product of monoids @s@ and @m@, which is a monoid
+--   when @m@ acts on @s@. Structurally, the semi-direct product is
+--   just a pair @(s,m)@.  However, the monoid instance is different.
+--   In particular, we have
+--
+-- > (s1,m1) <> (s2,m2) = (s1 <> (m1 `act` s2), m1 <> m2)
+--
+--   We call the monoid @m@ the quotient monoid and the monoid @s@ the
+--   sub-monoid of the semi-direct product. The semi-direct product
+--   @Semi s m@ is an extension of the monoid @s@ with @m@ being the
+--   quotient.
 data Semi s m = Semi s !m
 
 
@@ -33,7 +38,7 @@ instance (Monoid m, Monoid s, Action m s) => Monoid (Semi s m) where
   {-# INLINE mappend #-}
   mconcat                           = foldr mappend mempty
   {-# INLINE mconcat #-}
-  
+
 -- | The quotient map.
 quotient :: Semi s m -> m
 quotient (Semi _ m) = m
@@ -43,8 +48,8 @@ inject :: Monoid m => s -> Semi s m
 inject = flip Semi mempty
 
 -- | The semi-direct product gives a split extension of @s@ by
--- @m@. This allows us to embed @m@ into the semi-direct product. This
--- is the embedding map. The quotient and embed maps should satisfy
--- the equation @quotient . embed = id@.
+--   @m@. This allows us to embed @m@ into the semi-direct
+--   product. This is the embedding map. The quotient and embed maps
+--   should satisfy the equation @quotient . embed = id@.
 embed :: Monoid s => m -> Semi s m
 embed = Semi mempty
