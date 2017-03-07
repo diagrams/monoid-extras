455e250ff65bc41495e96a717b86454bf2234e21
Merijn Verstraaten <merijn@inconsistent.nl>
2017-03-07 15:35:18 +0100

    Add missing instances for Inf.

--- src/Data/Monoid/Inf.hs
+++ src/Data/Monoid/Inf.hs
@@ -31,13 +31,14 @@ module Data.Monoid.Inf
        , posFinite, negFinite
        ) where
 
+import           Control.Applicative (liftA2)
 import           Data.Data
 import           Data.Semigroup
-import           Prelude          hiding (maximum, minimum)
-import qualified Prelude          as P
+import           Prelude             hiding (maximum, minimum)
+import qualified Prelude             as P
 
-import           Data.Foldable    (Foldable)
-import           Data.Traversable (Traversable)
+import           Data.Foldable       (Foldable)
+import           Data.Traversable    (Traversable)
 
 data Pos
 data Neg
@@ -74,6 +75,24 @@ instance Ord a => Monoid (Inf Pos a) where
 instance Ord a => Monoid (Inf Neg a) where
   mempty = Infinity
   mappend = (<>)
+
+instance Applicative (Inf p) where
+    pure = Finite
+    Infinity <*> _ = Infinity
+    _ <*> Infinity = Infinity
+    Finite f <*> Finite x = Finite $ f x
+
+instance Monad (Inf p) where
+    Infinity >>= _ = Infinity
+    Finite x >>= f = f x
+
+instance Bounded a => Bounded (NegInf a) where
+    minBound = Infinity
+    maxBound = Finite maxBound
+
+instance Bounded a => Bounded (PosInf a) where
+    minBound = Finite minBound
+    maxBound = Infinity
 
 minimum :: Ord a => [a] -> PosInf a
 minimum xs = P.minimum (Infinity : map Finite xs)
